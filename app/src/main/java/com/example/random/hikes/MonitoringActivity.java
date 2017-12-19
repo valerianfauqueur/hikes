0217666c14463811aace4186d04aa91c832f41a6
Raphael Zerbib <raphael.zerbib@hetic.net>
2017-12-19 22:37:30 +0100

    [page/monitoring] add flexbox dependencies and front

---
+++ app/src/main/java/com/example/random/hikes/MonitoringActivity.java
@@ -0,0 +1,13 @@
+package com.example.random.hikes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonitoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitoring);
+    }
+}
