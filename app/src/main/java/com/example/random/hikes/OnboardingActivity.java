2ece08d7fe9d04e89c440e694309016c3731a07e
valerianfauqueur <valerian.fauqueur@hetic.net>
2017-12-19 16:06:26 +0100

    Add onboarding

---
+++ app/src/main/java/com/example/random/hikes/OnboardingActivity.java
@@ -0,0 +1,278 @@
+package com.example.random.hikes;
+
+import android.animation.ArgbEvaluator;
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.random.hikes.Utils;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    ImageButton mNextBtn;
+    Button mSkipBtn, mFinishBtn;
+
+    ImageView zero, one, two;
+    ImageView[] indicators;
+
+    int lastLeftValue = 0;
+
+    CoordinatorLayout mCoordinator;
+
+
+    static final String TAG = "PagerActivity";
+
+    int page = 0;   //  to track page position
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mNextBtn = (ImageButton) findViewById(R.id.intro_btn_next);
+        mSkipBtn = (Button) findViewById(R.id.intro_btn_skip);
+        mFinishBtn = (Button) findViewById(R.id.intro_btn_finish);
+
+        zero = (ImageView) findViewById(R.id.intro_indicator_0);
+        one = (ImageView) findViewById(R.id.intro_indicator_1);
+        two = (ImageView) findViewById(R.id.intro_indicator_2);
+
+        mCoordinator = (CoordinatorLayout) findViewById(R.id.main_content);
+
+        indicators = new ImageView[]{zero, one, two};
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.setCurrentItem(page);
+        updateIndicators(page);
+
+        final int color1 = ContextCompat.getColor(this, R.color.cyan);
+        final int color2 = ContextCompat.getColor(this, R.color.orange);
+        final int color3 = ContextCompat.getColor(this, R.color.green);
+
+        final int[] colorList = new int[]{color1, color2, color3};
+        final ArgbEvaluator evaluator = new ArgbEvaluator();
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                /*
+                color update
+                 */
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                page = position;
+
+                updateIndicators(page);
+
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+
+
+                mNextBtn.setVisibility(position == 2 ? View.GONE : View.VISIBLE);
+                mFinishBtn.setVisibility(position == 2 ? View.VISIBLE : View.GONE);
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page += 1;
+                mViewPager.setCurrentItem(page, true);
+            }
+        });
+
+        mSkipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+                Intent LoginIntent = new Intent(OnboardingActivity.this, MainActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
+
+        mFinishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  update 1st time pref
+                Utils.saveSharedSetting(OnboardingActivity.this, MainActivity.PREF_USER_FIRST_TIME, "false");
+                // start login activity
+                finish();
+                Intent LoginIntent = new Intent(OnboardingActivity.this, MainActivity.class);
+                startActivity(LoginIntent);
+
+            }
+        });
+
+    }
+
+    void updateIndicators(int position) {
+        for (int i = 0; i < indicators.length; i++) {
+            if(i == position) {
+                indicators[i].getBackground().setAlpha(255);
+            } else {
+                indicators[i].getBackground().setAlpha(76);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_onboarding, menu);
+        return true;
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        ImageView img;
+
+        int[] bgs = new int[]{R.drawable.first, R.drawable.second, R.drawable.third};
+        String[] titles = {"POSTEZ VOS ANNONCES !", "RECOMMANDEZ-LES !", "RECHERCHEZ UNE GARDE !"};
+        String[] contents = {
+                "Emensis itaque difficultatibus multis et nive obrutis callibus plurimis ubi prope Rauracum ventum est ad supercilia fluminis Rheni",
+                "Emensis itaque difficultatibus multis et nive obrutis callibus plurimis ubi prope Rauracum ventum est ad supercilia fluminis Rheni",
+                "Emensis itaque difficultatibus multis et nive obrutis callibus plurimis ubi prope Rauracum ventum est ad supercilia fluminis Rheni"
+        };
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_onboarding, container, false);
+            TextView TitleView = (TextView) rootView.findViewById(R.id.section_label);
+            TextView ContentView = (TextView) rootView.findViewById(R.id.section_content);
+            TitleView.setText(getString(R.string.section_title, titles[getArguments().getInt(ARG_SECTION_NUMBER) - 1]));
+            ContentView.setText(getString(R.string.section_content, contents[getArguments().getInt(ARG_SECTION_NUMBER) - 1]));
+
+            img = (ImageView) rootView.findViewById(R.id.section_img);
+            img.setImageResource(bgs[getArguments().getInt(ARG_SECTION_NUMBER) - 1]);
+
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+
+}
