f4226a4bed9e7d2283dd2748588a0e3ac59fe6e6
valerianfauqueur <valerian.fauqueur@hetic.net>
2017-12-19 16:06:26 +0100

    Add onboarding

--- app/src/main/java/com/example/random/hikes/MainActivity.java
+++ app/src/main/java/com/example/random/hikes/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.random.hikes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.random.hikes.Utils;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PREF_USER_FIRST_TIME = "user_first_time";
+    boolean isUserFirstTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        isUserFirstTime = Boolean.valueOf(Utils.readSharedSetting(MainActivity.this, PREF_USER_FIRST_TIME, "true"));
+        Intent introIntent = new Intent(MainActivity.this, OnboardingActivity.class);
+        introIntent.putExtra(PREF_USER_FIRST_TIME, isUserFirstTime);
+
+        if (isUserFirstTime)
+            startActivity(introIntent);
+
+        if(!isUserFirstTime)
+            setContentView(R.layout.activity_main);
     }
 }
